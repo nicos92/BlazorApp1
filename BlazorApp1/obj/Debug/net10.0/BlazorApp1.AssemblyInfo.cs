c0ed03290e8e247b62e89fecf8c65e51553d0b05
Nicolas sandoval <nicolassandoval140692@gmail.com>
2026-04-03 18:04:55 -0300

    arregla la validacion de los datos

--- BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e1f6ba6b951c4a5b45eb0a96831189647b70e39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5be8190277bfa5db749160f1a598836cc53492")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
